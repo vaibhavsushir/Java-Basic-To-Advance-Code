c7fbf4f494c43eebbf2cace4aaccfd7d4dadb8e0
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-02-26 08:12:43 +0530

    Update 3 singleLine.java

--- 3 singleLine.java
+++ 3 singleLine.java
@@ -1,8 +1,9 @@
 class singleLine {
     public static void main(String[] args){
-        System.out.print("Print Pattern Using Single Line") 
+        System.out.print("Print Pattern Using Single Line");
         System.out.print("*\n* *\n* * *\n* * * *\n* * * * *");
         System.out.print("          \n\n*                 \n          *   *\n         *   *   *\n *   *   *   *\n*    *   *   *   *");
     }
 }
+
 
