99b6fae09f321a7a68ce4fb2a08801e475a4a498
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-01 07:06:24 +0530

    Add files via upload

---
+++ 11.TriangleArea.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class TriangleArea {
+    public static void main(String[]args){
+        System.out.println("Calulate The Triangle Area");
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter Width:");
+        int a=input.nextInt();
+        System.out.print("Enter Height: ");
+        int b=input.nextInt();
+        double area=0.5*a*b;
+        System.out.println("Area Of Cicle is: "+ area);
+    }
+}
