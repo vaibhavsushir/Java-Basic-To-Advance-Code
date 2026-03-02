e1c391bf4f3d4cde4293e5d49c2b99f661c953d1
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-02 09:41:31 +0530

    Add files via upload

---
+++ 16.OddEven.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class OddEven {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Check Odd Even Number Checker");
+        Scanner input=new Scanner(System.in);;
+        System.out.print("Enter The Number: ");
+        int a=input.nextInt();;
+        if(a % 2 == 0){
+            System.out.println("Even Number");
+        }else{
+            System.out.println("Odd Number");
+        }
+    }
+}
