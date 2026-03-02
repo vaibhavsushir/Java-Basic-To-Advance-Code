ed1d7c519e783f89ded157c6b904917673f3fd14
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-02 09:40:09 +0530

    Add files via upload

---
+++ 14.FehrenheitToCelsius.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class FehrenheitToCelsius {
+    public static void main(String[]args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Welcome To Temperature Converter");
+        System.out.print("Enter Fehrenheit Temperature: ");
+        float a=input.nextInt();
+        float temp=(a-32)*5/9;;
+        System.out.println("Temperature In Celsius: "+ temp+" cel");
+
+    }
+}
