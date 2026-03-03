a7a940a4915d9edcdebe337a1be33395441d8779
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-03 08:33:56 +0530

    Update 16.GreatestNumber.java

--- 16.GreatestNumber.java
+++ 16.GreatestNumber.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner ;
 class GreatestNumber {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
@@ -18,3 +18,4 @@ class GreatestNumber {
         }
     }
 }
+
