d9b62331501db7801939282e2667dc3ae840d33f
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-04 07:21:01 +0530

    Add files via upload

---
+++ 19.Grading.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Grading {
+    public static void main(String[]args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter To Grade Calculator");
+        System.out.print("Enter Your Percent marks: ");
+        int a=input.nextInt();;
+
+        if(a>=90){
+            System.out.println("Grade,You Have Got A");
+        }
+        else if(a>=75){
+            System.out.println("Grade,You Have Got B");
+        }else if(a>=60){
+            System.out.println("Grade,You Have Got C");
+        }else if(a>=30){
+            System.out.println("Grade,You Have Got D");
+        }else{
+            System.out.println("Sorry,You Are Fail Don't Quit Try Again");
+        }
+    }
+}
