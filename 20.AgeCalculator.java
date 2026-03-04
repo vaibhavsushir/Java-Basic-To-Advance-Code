d181f5568496e805c9a3baf0d5d58ef2d9da2fb2
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-04 07:22:41 +0530

    Add files via upload

---
+++ 20.AgeCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class AgeCalculator {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Age Calculator");
+        Scanner input=new Scanner(System.in);
+        System.out.print("Please,Enter Your Age: ");
+        int a=input.nextInt();;
+        if(a>=65){
+            System.out.println("You Are Senior Citizen");
+        }else if(a>=20){
+            System.out.println("You Are Adult");
+        }else if(a>=13){
+            System.out.println("You Are Teenager");
+        }else{
+            System.out.println("You Are child");
+        }
+    }
+}
