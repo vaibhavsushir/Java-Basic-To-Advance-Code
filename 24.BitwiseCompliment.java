e44cfa6c567ea765ecb9f836adc4956400e41f4f
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-08 07:55:07 +0530

    Add files via upload

---
+++ 24.BitwiseCompliment.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class BitwiseCompliment {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome To Showcasing Compliment operators");
+        System.out.print("Enter Your Number: ");
+        int a = input.nextInt();
+        ;
+        int result = ~a;
+        System.out.println(result);
+    }
+}
