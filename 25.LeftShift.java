977e1bf69ab1e5cfd29c61041f259fa4d2eaefbc
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-09 09:54:01 +0530

    Add files via upload

---
+++ 25.LeftShift.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class LeftShift {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome To Showcasing LeftShift operators");
+        System.out.print("Enter Your Number: ");
+        int a = input.nextInt();;
+        int result = a << 4;
+        System.out.println(result);
+    }
+}
