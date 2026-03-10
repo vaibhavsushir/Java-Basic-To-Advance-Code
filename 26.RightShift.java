2c05469d70d81ad8dd92e8b551a79d37c4768435
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-10 08:06:53 +0530

    Add files via upload

---
+++ 26.RightShift.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class RightShift {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome To Showcasing RightShift operators");
+        System.out.print("Enter Your Number: ");
+        int a = input.nextInt();;
+        int result = a >> 1;
+        System.out.println(result);
+    }
+}
