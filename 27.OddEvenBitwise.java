debd5433419507df8fee7c3400cc53f898ec0c4f
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-11 07:07:40 +0530

    Add files via upload

---
+++ 27.OddEvenBitwise.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class OddEvenBitwise {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Odd Even Bitwise Calculator");
+        Scanner input=new Scanner(System.in);;
+        System.out.print("Enter Your Number: ");
+        int a=input.nextInt();
+
+        if((a & 1) ==1){
+            System.out.println("Your Number Is Odd");
+        }else{
+            System.out.println("Your Number Is Even");
+        }
+
+    }
+
+}
