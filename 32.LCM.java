229e6eecd5bd70c73a3f87bcdfb27113fb13050e
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-28 10:52:30 +0530

    Add files via upload

---
+++ 32.LCM.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class LCM {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);;
+        System.out.println("WElcome To LCM Number Calculation");
+        System.out.print("Enter Your First Number: ");
+        int a=input.nextInt();
+        System.out.print("Enter Second Number: ");
+        int b=input.nextInt();;
+        int lcm=LCM(a,b);
+        System.out.println("LCM of Two Number Is:"+lcm);
+    }
+    public static int LCM(int a,int b) {
+        int i = 1;
+        while (true) {
+            int factor = b * i;
+            if (factor % a == 0) {
+                return factor;
+            }
+            i++;
+            ;
+        }
+    }
+}
