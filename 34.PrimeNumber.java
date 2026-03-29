5a416a42a3aee31c0bb4fe1b1a362b43f7199aa1
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-29 10:02:22 +0530

    Add files via upload

---
+++ 34.PrimeNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Welcome TO Prime Number Calculator");
+        System.out.println("Enter Your Number: ");
+        int a=input.nextInt();;
+        boolean isprime=IsPrime(a);;
+        if(isprime){
+            System.out.println("Your Number is prime");
+        }else{
+            System.out.println("Your NUmber Is NOt Prime");
+        }
+
+    }
+    public static boolean IsPrime(int a){
+        int i=2;
+        while(i<a){
+            if(a%i==0){
+                return false;
+            }
+            i++;;
+        }
+    return true;
+    }
+}
