9f68dba69c8ac80b352b567946d2b3f82fd738c1
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-29 10:01:21 +0530

    Add files via upload

---
+++ 33.GCD.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class GCD {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome To GCD");
+        System.out.println("Enter First Number: ");
+        int a = input.nextInt();
+        System.out.println("Enter Second Number: ");
+        int b = input.nextInt();
+    }
+}
+
+
