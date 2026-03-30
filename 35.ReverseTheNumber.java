beb4d2039e4f85cd077d180f339f3ec283074afd
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-03-30 09:28:06 +0530

    Add files via upload

---
+++ 35.ReverseTheNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class ReverseTheNumber {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("WElcome to Reverse The Digits");
+        System.out.print("Enter Your Number: ");
+        int num=input.nextInt();;
+        int Reverse=reverse(num);
+        System.out.println("Your Reverse Number Is: "+Reverse);
+    }
+    public static int reverse(int num){
+        int newnumber=0;
+        while(num>0){
+            int digit=num % 10;
+            newnumber=num*10+digit;;
+            num /= 10;;
+        }
+        return newnumber;
+    }
+}
