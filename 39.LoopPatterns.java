f2c1a3d82a57a5bb845bd50c0051d29a667f7c48
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-01 11:10:37 +0530

    Add files via upload

---
+++ 39.LoopPatterns.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class LoopPatterns {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Welcome To Pattern Print");
+        System.out.print("Enter Number Of Rows: ");
+        int rows = input.nextInt();
+    }
+}
