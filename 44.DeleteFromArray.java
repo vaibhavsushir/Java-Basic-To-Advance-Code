8bb78aa9a46effbfc87aa645b8258db0055ddb37
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-04 09:15:04 +0530

    Add files via upload

---
+++ 44.DeleteFromArray.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class DeleteFromArray {
+    public static void main(String[]args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Welcome To Array Deletion\n");
+        int[] numarray=ArrayUtility.inputArray();
+        System.out.print("Enter The Element You Want To Delete: ");
+        int numtodelete=input.nextInt();
+
+        System.out.println("Here Is Your New Array");
+    }
+
+    }
+
