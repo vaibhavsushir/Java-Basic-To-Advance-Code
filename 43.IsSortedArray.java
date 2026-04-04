b0d5fa1ea747b69ab245463b500d6ab14dace660
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-04 09:13:56 +0530

    Add files via upload

---
+++ 43.IsSortedArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class IsSortedArray {
+    public static void main(String[]args){
+        System.out.println("Welcome To Array Sorting Check Post\n");
+        int[] numarray=ArrayUtility.inputArray();
+        boolean IsInc = IsIncreasing(numarray);
+        boolean IsDec= IsDecreasing(numarray);
+        if(IsInc || IsDec){
+            System.out.println("Array Is Sorted");
+        }else{
+            System.out.println("Array Is Not Sorted");
+        }
+    }
+    public static boolean IsIncreasing(int[] numarray){
+        int i=1;
+        while(i < numarray.length){
+        if(numarray[i] < numarray[i-1]){
+            return false;
+        }
+        i++;
+    }
+        return true ;
+    }
+    public static boolean IsDecreasing(int[] numarray) {
+        int i = 1;
+        while (i < numarray.length) {
+            if (numarray[i] > numarray[i - 1]) {
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
