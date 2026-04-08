93501a7e78185b3e87997d48e7e5cc71cbc9a1a1
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-08 08:17:41 +0530

    Add files via upload

---
+++ 46.PalindromeArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class PalindromeArray {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Palindrome Checker\n");
+        int[] numarr = ArrayUtility.inputArray();
+        boolean ispalin = IsPalindrome(numarr);
+        if(ispalin){
+            System.out.println("Your Array Is Palindrome");
+        }else {
+            System.out.println("Your Array Is Not Palindrome");
+        }
+    }
+    public static boolean IsPalindrome(int[] numarr){
+        int i = 0;
+        while(i < numarr.length/2){
+            if(numarr[i] == numarr[(numarr.length-1)-i]){
+                i++;;
+            }
+
+        }
+
+        return true;
+    }
+}
