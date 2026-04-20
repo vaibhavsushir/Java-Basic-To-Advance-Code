af09fb1ac0c3428fa8b598a60adde1ddd773e3f6
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-20 09:25:21 +0530

    Add files via upload

---
+++ 45.ReverseArray.java
@@ -0,0 +1,20 @@
+class ReverseArray {
+    public static void main(String[]args){
+        System.out.println("Welcome To Array Reversal\n");
+        int[] numarray=ArrayUtility.inputArray();
+        reverse(numarray);
+        System.out.println("Your Reverse Array Is: ");
+        ArrayUtility.displayArray(numarray);
+    }
+    public static void reverse(int[] arr){
+        int i=0;
+        while(i < arr.length / 2){
+            int swap = arr[i];
+            arr[i] = arr[(arr.length - 1)-i] ;
+            arr[(arr.length-1)-i] = swap ;
+            i++;
+        }
+
+    }
+
+}
