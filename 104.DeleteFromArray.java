0101c32bf9a3db3e5d1feba67b69db500dd2a8be
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-20 09:28:52 +0530

    Update 104.DeleteFromArray.java

--- 104.DeleteFromArray.java
+++ 104.DeleteFromArray.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class DeleteFromArray {
     public static void main(String[]args){
-        Scanner input=new Scanner(System.in);
+        Scanner input=new Scanner(System.in); 
         System.out.println("Welcome To Array Deletion\n");
         int[] numarray=ArrayUtility.inputArray();
         System.out.print("Enter The Element You Want To Delete: ");
