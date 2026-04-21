97964656257941cbefa3104f627e755b002df198
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-21 09:47:09 +0530

    Add files via upload

---
+++ 47.MergetoSortedArray.java
@@ -0,0 +1,29 @@
+class MergetoSortedArray {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Merge Two Sorted Array\n");
+        int[] arr1 = ArrayUtility.inputArray();
+        int[] arr2= ArrayUtility.inputArray();
+        int[] mergearr = merge(arr1,arr2);
+        System.out.println("Your Merge Array Is: ");
+        ArrayUtility.displayArray(mergearr);
+    }
+    public static int[] merge(int[] arr1,int[] arr2){
+        int newsize = arr1.length + arr2.length;
+        int[] newarr = new int[newsize];
+        int i = 0,j=0,k=0;
+
+        while(i < arr1.length || i < arr2.length){
+
+            if(j == arr2.length || (i < arr1.length) && arr1[i] < arr2[j]){
+                newarr[k] = arr1[i] ;
+                i++;
+                k++;
+            }else{
+                newarr[k] = arr2[j] ;
+                j++;
+                k++;
+            }
+        }
+        return newarr;
+    }
+}
