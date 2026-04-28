4cd37b9c1d222f3db48afd92ef6d5a5444b2cfa9
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-28 08:22:50 +0530

    Add files via upload

---
+++ 52.Course.java
@@ -0,0 +1,27 @@
+class Course {
+    static int maxcapacity = 100;
+
+    String coursename;
+    int enrollments;
+    String[] enrolledstudents = new String[maxcapacity];
+    Course(String coursename){
+            this.coursename = coursename;
+            this.enrollments = 0;
+            this.enrolledstudents = new String[maxcapacity];
+    }
+
+
+
+
+    static void setMaxcapacity(int maxcapacity){
+        Course.maxcapacity =maxcapacity;
+    }
+    void enrollstudents(String studentname){
+        enrolledstudents[enrollments] = studentname;
+        enrollments++;
+    }
+    void unrolledstudent(String studentname){
+        System.out.println("StudentName Removed");
+        enrollments++;;
+    }
+}
