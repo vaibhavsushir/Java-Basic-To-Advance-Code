cd1ecd1fab5472782a9ea20240f5f7e13e9785cc
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-04-28 08:23:14 +0530

    Add files via upload

---
+++ 53.MinimumTernary.java
@@ -0,0 +1,17 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+class MinimumTernary {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Find Minimum Number\n");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your First Number: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter Second Number: ");
+        int num2 = input.nextInt();
+
+        int greatestnumber = num1 > num2 ? num1 : num2;;
+        System.out.println(greatestnumber+ " Is Greatest Number");
+
+    }
+}
