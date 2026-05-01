02dd5bd90bedb1402b6e7834a55f18120406c50d
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-05-01 08:58:50 +0530

    Add files via upload

---
+++ 54.OddEvenTernary.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class OddEvenTernary {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome To Odd Even Number Checker\n");
+        System.out.print("Enter Your NUmber: ");
+        int num = input.nextInt();
+        String result =num % 2 ==0 ? "Even" : "Odd" ;
+        System.out.print("Your Number Is : "+result);
+
+
+    }
+}
