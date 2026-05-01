e8a2b9cea8096d283a95c4ed47fb744994db98e4
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-05-01 08:59:10 +0530

    Add files via upload

---
+++ 55.AbsoluteTernary.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class AbsoluteTernary {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome To Absolute Value\n");
+        System.out.print("Enter Your Number: ");
+        int num = input.nextInt();
+        int result = num > 0 ? num : (-num);
+        System.out.println("Absolute Number Is:"+result);
+
+
+    }
+}
