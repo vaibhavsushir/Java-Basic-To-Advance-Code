333ae72cd16405793b95f4d692564fe2d421fa71
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-05-01 08:59:43 +0530

    Fix formatting issues in OddEvenTernary.java

--- 114.OddEvenTernary.java
+++ 114.OddEvenTernary.java
@@ -4,7 +4,7 @@ class OddEvenTernary {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome To Odd Even Number Checker\n");
         System.out.print("Enter Your NUmber: ");
-        int num = input.nextInt();
+        int num = input.nextInt(); 
         String result =num % 2 ==0 ? "Even" : "Odd" ;
         System.out.print("Your Number Is : "+result);
 
