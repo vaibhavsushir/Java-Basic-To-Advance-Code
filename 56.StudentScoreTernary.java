697224d13fd1ac88800e4f0b314226a7d7fefba2
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-05-02 10:03:44 +0530

    Add files via upload

---
+++ 56.StudentScoreTernary.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class StudentScoreTernary {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome To Categories Student Score\n");
+        System.out.print("Enter Your Score: ");
+        int score = input.nextInt();
+
+        String result = score > 80 ? "High" : (score > 50 ? "Moderate" : "Low");
+
+        System.out.println("Your Grade Is: " + result);
+    }
+}
