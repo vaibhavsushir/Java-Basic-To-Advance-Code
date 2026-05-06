7455d2e3ba2afe10557f7839aa9f5d501df2c2bc
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-05-06 12:57:10 +0530

    Update 05.Sum.java

--- 05.Sum.java
+++ 05.Sum.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
     class Sum {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); 
         System.out.print("Enter First no: ");
         int Fnumber = input.nextInt();
         System.out.print("Enter second no: ");
