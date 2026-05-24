76aba40ad1c254a4d9cc1c55d2e9ccef9dde3c87
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-05-24 11:07:28 +0530

    Update C4.Welcome.java

--- C4.Welcome.java
+++ C4.Welcome.java
@@ -3,7 +3,7 @@ class Welcome {
     public static void main(String[] args){
         System.out.print("Please Enter Your Name: ");
         Scanner input = new Scanner(System.in);
-        String name = input.nextLine();
+        String name = input.nextLine(); 
         System.out.println("Welcome "+ name +" to Kg Coding");
     }
 }
