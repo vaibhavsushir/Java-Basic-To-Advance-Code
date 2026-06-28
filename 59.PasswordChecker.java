157733cd7f9e53b63122836b54c0c57fba74077b
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-06-28 09:08:42 +0530

    Add files via upload

---
+++ 59.PasswordChecker.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class PasswordChecker {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome To Password Checker\n");
+        String password;
+    do{
+        System.out.print("Please,Enter Your Password: ");
+        password = input.next();
+    }while(!IsValid(password));
+        System.out.println("Thanks For Entering Valid Password");
+
+    }
+    public static boolean IsValid(String password){
+        return password.length() > 6;
+    }
+
+}
