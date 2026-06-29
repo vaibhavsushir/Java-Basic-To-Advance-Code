6f45b007589c4161f2f398244209addc6beca19d
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-06-29 10:47:37 +0530

    Add files via upload

---
+++ 60.NumberGuessingGame.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome To Number Guessing Game\n");
+        int num=5,guess;
+        do{
+            System.out.print("Please,Guess Number Between 0 To 10: ");
+            guess = input.nextInt();
+        }while(num != guess);
+        System.out.println("You Have Successfully Guess The Number");
+
+
+    }
+}
