847cddc5e46ab9295ec0cffbdc30c4a0f8037726
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-02 07:28:26 +0530

    Update 010.TriangleArea.java

--- 010.TriangleArea.java
+++ 010.TriangleArea.java
@@ -5,7 +5,7 @@ class TriangleArea{
         Scanner input=new Scanner(System.in);
         System.out.print("Enter Width:");
         int a=input.nextInt();
-        System.out.print("Enter Height: ");
+        System.out.print("Enter Height: "); 
         int b=input.nextInt();
         double area=0.5*a*b;
         System.out.println("Area Of Cicle is: "+ area);
