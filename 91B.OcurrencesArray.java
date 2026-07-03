2e212d46fe7f908c2c189c3116ab9274ec3a4bd1
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-03 15:23:11 +0530

    Refactor OcurrencesArray class structure

--- 91B.OcurrencesArray.java
+++ 91B.OcurrencesArray.java
@@ -9,7 +9,7 @@ class OcurrencesArray {
         int occurence = occurence(num,numarray);
         System.out.print("The Number Is Found " + occurence + " times");
     }
-    public static int occurence(int num, int[] numarray) {
+    public static int occurence(int num, int[] numarray){
         int occ=0;
         int i = 0;
         while (i < numarray.length) {
