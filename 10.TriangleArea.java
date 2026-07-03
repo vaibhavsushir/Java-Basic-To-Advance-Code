9af742a4f488af72a906d0b7383fac8ecadfea88
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-03 15:21:48 +0530

    Update 10.TriangleArea.java

--- 10.TriangleArea.java
+++ 10.TriangleArea.java
@@ -8,6 +8,7 @@ class TriangleArea{
         System.out.print("Enter Height: "); 
         int b=input.nextInt(); 
         double area=0.5*a*b;
+        
         System.out.println("Area Of Cicle is: "+ area);
     } 
 }
