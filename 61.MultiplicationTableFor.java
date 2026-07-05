2c74a070c828263e8f7fdf9d047ad396db255db1
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-05 07:35:08 +0530

    Add files via upload

---
+++ 61.MultiplicationTableFor.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class MultiplicationTableFor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome To Multiplication Table Printer\n");
+        System.out.print("Enter Number You Want To Print Multiplication Table: ");
+        int num = input.nextInt();
+
+        for(int i=1; i <= 10; i++){
+            int table = i * num;
+            System.out.println(i + " * " + num + " = "+ table);
+        }
+
+
+    }
+}
