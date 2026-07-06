6536914719b60455fdc8c7f145064b646a87f98e
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-06 07:37:47 +0530

    Add files via upload

---
+++ 62.PrimeFor.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class PrimeFor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcoe To Check Number Is Prime Or Not\n");
+        System.out.print("Enter Your Number: ");
+        int num = input.nextInt();
+        System.out.print("Your Number Is "+ (IsPrime(num) ? "Prime" : "Not Prime"));
+    }
+        public static boolean IsPrime(int num){
+                    for(int i=2 ; i < num ; i++){
+                        if(num % i == 0){
+                          return false;
+                }
+
+        }
+            return true;
+    }
+}
