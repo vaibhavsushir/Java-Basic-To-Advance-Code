35bfb88355f74f45570901852112a586b9515637
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-09 08:17:58 +0530

    Add files via upload

---
+++ 65.ExitOnExit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class ExitOnExit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while(true){
+            System.out.print("Enter Your Command: ");
+            String command = input.next();
+            if( command.equals("Exit")){
+                break;
+            }
+        }
+        System.out.println("You Have Successfully Exit");
+    }
+}
