ed5b5074a5830d42ee9a2641ea09ca6ecf3b5808
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-09 08:18:58 +0530

    Fix formatting issues in ExitOnExit.java

--- A125.ExitOnExit.java
+++ A125.ExitOnExit.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 class ExitOnExit {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
         while(true){
             System.out.print("Enter Your Command: ");
             String command = input.next();
