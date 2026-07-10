924c6e2bb011b08a4ee791eaf333e13ce8df5709
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-10 08:05:04 +0530

    Add files via upload

---
+++ 66.SumPositive.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class SumPositive {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Sum Of Positive Number\n");
+        int[] numarr = ArrayUtility.inputArray();
+
+        int sum = 0;
+        for( int num : numarr){
+            if ( num < 0){
+                continue;
+            }else{
+                sum += num ;
+            }
+        }
+        System.out.println("Sum Of Positive Number Is: "+ sum);
+
+    }
+}
