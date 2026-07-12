d6c2855fc3fd913857c2b575b92c0be4e56887e1
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-12 07:42:08 +0530

    Update A120.NumberGuessingGame.java

--- A120.NumberGuessingGame.java
+++ A120.NumberGuessingGame.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class NumberGuessingGame {
+class NumberGuessingGame { 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome To Number Guessing Game\n");
