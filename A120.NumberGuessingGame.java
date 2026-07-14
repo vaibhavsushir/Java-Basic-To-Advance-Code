a98ab5804826ae4f3f1fdcb9d187aecbd5c9234f
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-14 07:26:57 +0530

    Update A120.NumberGuessingGame.java

--- A120.NumberGuessingGame.java
+++ A120.NumberGuessingGame.java
@@ -3,7 +3,7 @@ class NumberGuessingGame {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome To Number Guessing Game\n");
-        int num=5,guess;
+        int num=5,guess; 
         do{
             System.out.print("Please,Guess Number Between 0 To 10: ");
             guess = input.nextInt();
