3f513420b9a25270ca584cc5f025c83ee25edf41
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-14 07:24:12 +0530

    Add files via upload

---
+++ 70.StudentToString.java
@@ -0,0 +1,27 @@
+class StudentToString {
+    String name;
+
+    int age;
+
+    String rollnumber;
+
+    String house;
+
+    public StudentToString(String name, int age, String rollnumber, String house) {
+        this.name = name;
+        this.age = age;
+        this.rollnumber = rollnumber;
+        this.house = house;
+    }
+
+    @Override
+    public String toString() {
+        return "Student Details: {Name: " + name + ",Age: " + age + ",Roll Number: " + rollnumber + ",House: " + house + " }";
+
+    }
+
+    public static void main(String[] args) {
+        StudentToString student = new StudentToString("Vaibhav", 30, "56", "Shivaji");
+        System.out.println(student);
+    }
+}
