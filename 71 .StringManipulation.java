5f7a032f081a64a7ae22a06f80f03dec44019628
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-15 07:27:44 +0530

    Add files via upload

---
+++ 71 .StringManipulation.java
@@ -0,0 +1,13 @@
+class StringManipulation {
+    public static void main(String[] args) {
+        String  firstname = "prashant";
+        String lastname = "Jain";
+       // String fullname = firstname + " " + lastname;
+        String fullname = firstname.concat( " " ).concat( lastname);
+        System.out.println(fullname.toUpperCase());
+
+    }
+
+
+
+}
