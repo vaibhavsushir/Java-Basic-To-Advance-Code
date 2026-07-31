01a2056ec3af739a7675ddd79d8f0de5ccec2184
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-07-31 23:04:41 +0530

    Update A105.ReverseArray.java

--- A105.ReverseArray.java
+++ A105.ReverseArray.java
@@ -10,8 +10,8 @@ class ReverseArray {
         int i=0;
         while(i < arr.length / 2){
             int swap = arr[i];
-            arr[i] = arr[(arr.length - 1)-i] ;
-            arr[(arr.length-1)-i] = swap ;
+            arr[i] = arr[(arr.length - 1)-i];
+            arr[(arr.length-1)-i] = swap;
             i++;
         }
     }
