b98c9ce27b4274152215662fae4dc7d34631f520
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-06 23:25:27 +0530

    Update 21.BitwiseAnd.java

--- 21.BitwiseAnd.java
+++ 21.BitwiseAnd.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class BitwiseAnd {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        System.out.println("Welcome To Showcasing Bitwise And Operators");
+        System.out.println("Welcome To Showcasing Bitwise And Operators/n");
         System.out.print("Enter First Number: ");
         int a=input.nextInt();
         System.out.print("Enter Second Number: ");
