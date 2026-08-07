683e460466a3cb818ddc8b84ad6cfe4088428da0
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-07 23:20:22 +0530

    Update 08.FloatMultiplication.java

--- 08.FloatMultiplication.java
+++ 08.FloatMultiplication.java
@@ -1,4 +1,4 @@
- import java.util.Scanner;
+  import java.util.Scanner;
 class FloatMultiplication {
     public static void main(String[]args){
         System.out.println("Multiplication of Float Number\n");
