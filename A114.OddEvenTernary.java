585cf5bb000dd467fb2343fc104b276abb74925b
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-08 23:16:52 +0530

    Update A114.OddEvenTernary.java

--- A114.OddEvenTernary.java
+++ A114.OddEvenTernary.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class OddEvenTernary {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome To Odd Even Number Checker\n");
+        System.out.println("Welcome To Odd Even Number Checker");
         System.out.print("Enter Your NUmber: ");
         int num = input.nextInt(); 
         String result =num % 2 ==0 ? "Even" : "Odd" ;
