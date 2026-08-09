2182cf726c9040fbdca40d9907dd29b0ffbd19f0
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-09 23:03:59 +0530

    Update A115.AbsoluteTernary.java

--- A115.AbsoluteTernary.java
+++ A115.AbsoluteTernary.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class AbsoluteTernary {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome To Absolute Value\n");
+        System.out.println("Welcome To Absolute Value");
         System.out.print("Enter Your Number: ");
         int num = input.nextInt();
         int result = num > 0 ? num : (-num);
