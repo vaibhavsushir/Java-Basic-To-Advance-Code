47c2b0b806ed2127e062decd3cdef53fd685ffd3
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-10 23:01:13 +0530

    Update 22.BitwiseOr.java

--- 22.BitwiseOr.java
+++ 22.BitwiseOr.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class BitwiseOr {
+class BitwiseOr{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
         System.out.println("Welcome To Showcasing Bitwise And Operators");
