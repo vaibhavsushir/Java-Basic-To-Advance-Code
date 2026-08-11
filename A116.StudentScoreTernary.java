ea5e3e98fc2f13fc82205ed2c5566cfc36632a8c
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-11 23:22:58 +0530

    Update A116.StudentScoreTernary.java

--- A116.StudentScoreTernary.java
+++ A116.StudentScoreTernary.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class StudentScoreTernary {
+class StudentScoreTernary{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome To Categories Student Score\n");
