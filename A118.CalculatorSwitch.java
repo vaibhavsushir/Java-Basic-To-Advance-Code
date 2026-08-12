134dda2676aa0b528028efe4ba707aa45c845edc
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-12 23:41:08 +0530

    Update A118.CalculatorSwitch.java

--- A118.CalculatorSwitch.java
+++ A118.CalculatorSwitch.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class CalculatorSwitch {
+class CalculatorSwitch{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome To Calculator\n");
