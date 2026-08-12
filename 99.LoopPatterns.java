67d254a274cf77fa38e1a0f9711efd13ee667660
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-12 23:41:42 +0530

    Update 99.LoopPatterns.java

--- 99.LoopPatterns.java
+++ 99.LoopPatterns.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class LoopPatterns{
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        System.out.println("Welcome To Pattern Print");
+        System.out.println("Welcome To Pattern Print/n");
         System.out.print("Enter Number Of Rows: ");
         int rows = input.nextInt();
     } 
