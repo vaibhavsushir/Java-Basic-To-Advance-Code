11b587727e4bf8904d2b878fbcad15ddb1f297e8
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-13 22:45:41 +0530

    Update 15.OddEven.java

--- 15.OddEven.java
+++ 15.OddEven.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class OddEven {
+class OddEven{
     public static void main(String[] args) {
         System.out.println("Welcome To Check Odd Even Number Checker");
         Scanner input=new Scanner(System.in);;
