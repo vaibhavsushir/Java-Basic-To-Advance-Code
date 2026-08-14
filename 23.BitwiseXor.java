f5af771dd7d083c71066d5390ca366addc0fea54
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-14 22:30:38 +0530

    Update 23.BitwiseXor.java

--- 23.BitwiseXor.java
+++ 23.BitwiseXor.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class BitwiseXor {
+class BitwiseXor{
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.println("Welcome To Showcasing Bitwise And Operators");
