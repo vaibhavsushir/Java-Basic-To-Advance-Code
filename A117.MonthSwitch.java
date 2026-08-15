3bedfd288d16d7a74b4e7df45caa9d0122ef4775
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-15 23:15:30 +0530

    Update A117.MonthSwitch.java

--- A117.MonthSwitch.java
+++ A117.MonthSwitch.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class MonthSwitch {
+class MonthSwitch{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome To Month Switch\n");
