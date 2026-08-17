02f9655afc03b5818141e70cfac2926bec9f4cbb
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-17 23:48:25 +0530

    Update 13.FehrenheitToCelsius.java

--- 13.FehrenheitToCelsius.java
+++ 13.FehrenheitToCelsius.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class FehrenheitToCelsius {
+class FehrenheitToCelsius{
     public static void main(String[]args){
         Scanner input=new Scanner(System.in);
         System.out.println("Welcome To Temperature Converter/n");
