78fa8f41b66d0d9819c049ad57c0cfacfd0f274c
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-17 23:48:03 +0530

    Update 13.FehrenheitToCelsius.java

--- 13.FehrenheitToCelsius.java
+++ 13.FehrenheitToCelsius.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class FehrenheitToCelsius {
     public static void main(String[]args){
         Scanner input=new Scanner(System.in);
-        System.out.println("Welcome To Temperature Converter");
+        System.out.println("Welcome To Temperature Converter/n");
         System.out.print("Enter Fehrenheit Temperature:");
         float a=input.nextInt();
         float temp=(a-32)*5/9;;
