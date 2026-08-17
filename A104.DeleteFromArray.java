eab9bca7e88776eb8e53ddcdfced5e4b4e3028c4
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-17 23:48:48 +0530

    Update A104.DeleteFromArray.java

--- A104.DeleteFromArray.java
+++ A104.DeleteFromArray.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class DeleteFromArray {
+class DeleteFromArray{
     public static void main(String[]args){
         Scanner input=new Scanner(System.in); 
         System.out.println("Welcome To Array Deletion\n");
