23d22dc084f4141d5e7ffcc801ce84086cff386c
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-18 23:17:23 +0530

    Update 89.LoopPatterns.java

--- 89.LoopPatterns.java
+++ 89.LoopPatterns.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class LoopPatterns {
+class LoopPatterns{
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.println("Welcome To Pattern Print");
