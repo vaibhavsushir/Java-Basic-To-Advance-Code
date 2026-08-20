4980b0eb85cea3f050af37be88e6f9e53ed14efc
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-20 23:01:10 +0530

    Update A121.MultiplicationTableFor.java

--- A121.MultiplicationTableFor.java
+++ A121.MultiplicationTableFor.java
@@ -5,7 +5,6 @@ class MultiplicationTableFor {
         System.out.println("Welcome To Multiplication Table Printer\n");
         System.out.print("Enter Number You Want To Print Multiplication Table: ");
         int num = input.nextInt();
-
         for(int i=1; i <= 10; i++){
             int table = i * num;
             System.out.println(i + " * " + num + " = "+ table);
