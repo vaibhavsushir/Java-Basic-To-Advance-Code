1025cf7bee4aae0fdb4dcbaea905b5169c18001f
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-20 23:03:45 +0530

    Update 87.ArmstrongNumber.java

--- 87.ArmstrongNumber.java
+++ 87.ArmstrongNumber.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class ArmstrongNumber {
+class ArmstrongNumber{
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
         System.out.println("Welcome To Armstrong Number Calculator");
