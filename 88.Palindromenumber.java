0413fb908be426fb3164cfdce3eea45708dbfd95
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-26 22:30:53 +0530

    Update 88.Palindromenumber.java

--- 88.Palindromenumber.java
+++ 88.Palindromenumber.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 class Palindromenumber {
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-        System.out.println("WElcome to Palindrome Number");
+        System.out.println("WElcome to Palindrome Number\n");
         System.out.print("Enter Your Number: ");
- 
 }
-
 }
