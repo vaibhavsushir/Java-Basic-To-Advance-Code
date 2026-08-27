82dc11a3dbd20d0e44ae833772c0ad3084ff2baf
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-27 23:07:33 +0530

    Update 06.Swap.java

--- 06.Swap.java
+++ 06.Swap.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Swap {
+class Swap{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in) ;
         System.out.println("Welcome To Swaping Station\n");
