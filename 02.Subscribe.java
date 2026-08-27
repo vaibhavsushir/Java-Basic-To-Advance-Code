a5685c05ee5915bbfb7bb6aa36b48fb8bb4ac26c
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-27 23:06:07 +0530

    Correct newline in Subscribe output
    Fix newline character in output string.

--- 02.Subscribe.java
+++ 02.Subscribe.java
@@ -1,6 +1,6 @@
 class Subscribe {
     public static void main(String[] args) {
-        System.out.println("Print Subscribe");  
+        System.out.println("Print Subscribe/n");  
         System.out.println("Subscribe to KG Coding");
     }
 }
