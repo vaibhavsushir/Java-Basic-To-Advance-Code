6f5575c39b02ba344bb79107dc5b2184ca9c6d46
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-27 23:06:24 +0530

    Update 02.Subscribe.java

--- 02.Subscribe.java
+++ 02.Subscribe.java
@@ -1,4 +1,4 @@
-class Subscribe {
+class Subscribe{
     public static void main(String[] args){
         System.out.println("Print Subscribe/n");  
         System.out.println("Subscribe to KG Coding");
