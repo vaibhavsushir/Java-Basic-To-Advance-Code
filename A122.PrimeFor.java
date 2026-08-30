5ea9c8dea0ef7d5f74e70c1e1196844763ae438d
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-08-30 23:33:14 +0530

    Update A122.PrimeFor.java

--- A122.PrimeFor.java
+++ A122.PrimeFor.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class PrimeFor {
+class PrimeFor{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Welcoe To Check Number Is Prime Or Not\n");
