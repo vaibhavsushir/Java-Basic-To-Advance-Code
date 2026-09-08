9ec3eb82949b10f358a4ae62f6904bf7a6ac2b32
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-08 23:12:53 +0530

    Update 21.BitwiseAnd.java

--- 21.BitwiseAnd.java
+++ 21.BitwiseAnd.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class BitwiseAnd {
+class BitwiseAnd{
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.println("Welcome To Showcasing Bitwise And Operators/n");
