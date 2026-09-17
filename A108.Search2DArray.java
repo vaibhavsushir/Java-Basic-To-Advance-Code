cb06c6676f5c4efb7a37355758334f044e3e006b
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-17 23:22:56 +0530

    Update A108.Search2DArray.java

--- A108.Search2DArray.java
+++ A108.Search2DArray.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Search2DArray {
+class Search2DArray{
     public static void main(String[] args) { 
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome To 2D Search");
