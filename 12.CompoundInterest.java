fad051bcb351f9f7454370d816b3daddfd21ddef
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-21 08:00:40 +0530

    Update 12.CompoundInterest.java

--- 12.CompoundInterest.java
+++ 12.CompoundInterest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class CompoundInterest{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Welcome To Compound Interest Calculator\n");
         Scanner input=new Scanner(System.in);
         System.out.print("Enter Principle Amount: ");
