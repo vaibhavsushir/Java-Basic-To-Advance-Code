826d667f72ec5f7dfcd6f94d6a34e4b0dfabd4ad
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-24 22:15:00 +0530

    Update 02. numberRepeated.java

--- 02. numberRepeated.java
+++ 02. numberRepeated.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 class numberRepeated {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+     new Scanner(System.in);
         int[] numarr = new int[6];
       System.out.println("Enter 6 Elements: ");
         int num = input.nextInt();
