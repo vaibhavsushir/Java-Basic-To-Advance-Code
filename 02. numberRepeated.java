71584f2c4e9e062580df5488a910d9908918b62e
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-24 21:44:59 +0530

    Add files via upload

---
+++ 02. numberRepeated.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class numberRepeated {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] numarr = new int[6];
+        System.out.println("Enter 6 Elements: ");
+        int num = input.nextInt();
+        int add = numarr[0] + numarr[1];
+        System.out.println(add);
+    }
+}
