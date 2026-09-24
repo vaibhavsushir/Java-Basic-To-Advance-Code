7f8cbb7ea3348122e5f2521152c899a5f2f59fa8
Vaibhav Sushir <sushirvaibhav523@gmail.com>
2026-09-24 22:13:14 +0530

    Update 02. numberRepeated.java

--- 02. numberRepeated.java
+++ 02. numberRepeated.java
@@ -3,7 +3,7 @@ class numberRepeated {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] numarr = new int[6];
-        System.out.println("Enter 6 Elements: ");
+      System.out.println("Enter 6 Elements: ");
         int num = input.nextInt();
         int add = numarr[0] + numarr[1];
         System.out.println(add);
